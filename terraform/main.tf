2eaf0650fa81ebab0d3d495a61b7c43b0f24add3
storresruiz <storresruiz@cegid.com>
2025-10-23 12:46:56 +0200

    Refactor Terraform configuration to simplify S3 bucket setup and IAM group policy attachment; remove unused IAM policy outputs.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,53 +1,22 @@
-# S3 Bucket
-resource "aws_s3_bucket" "my_bucket" {
+# Data source para obtener el ID de la cuenta actual
+data "aws_caller_identity" "current" {}
+
+# S3 Bucket simplificado
+resource "aws_s3_bucket" "dev_academy_lab_bucket" {
   bucket = "lab-${data.aws_caller_identity.current.account_id}-personal"
 
   tags = {
-    Name        = "DevAcademyLabBucket"
-    Environment = var.environment
+    Name = "DevAcademyLabBucket"
   }
 }
 
 # IAM Group
-resource "aws_iam_group" "my_group" {
+resource "aws_iam_group" "dev_academy_lab_group" {
   name = "DevAcademyLabGroup"
 }
 
-# IAM Policy Document
-data "aws_iam_policy_document" "bucket_policy" {
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:ListBucket"
-    ]
-    resources = [
-      aws_s3_bucket.my_bucket.arn
-    ]
-  }
-
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject"
-    ]
-    resources = [
-      "${aws_s3_bucket.my_bucket.arn}/*"
-    ]
-  }
+# Usar política AWS gestionada en lugar de crear una personalizada
+resource "aws_iam_group_policy_attachment" "s3_access" {
+  group      = aws_iam_group.dev_academy_lab_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
-
-# IAM Policy
-resource "aws_iam_policy" "my_policy" {
-  name   = "DevAcademyBucketAccess"
-  policy = data.aws_iam_policy_document.bucket_policy.json
-}
-
-# Attach Policy to Group
-resource "aws_iam_group_policy_attachment" "policy_attachment" {
-  group      = aws_iam_group.my_group.name
-  policy_arn = aws_iam_policy.my_policy.arn
-}
-
-# Data source to get current AWS account ID
-data "aws_caller_identity" "current" {}
